79846938d8edb788b1419c1cd5eaeb9b12a7d4c9
hungnbph <hung19993112@gmail.com>
2020-09-26 08:36:02 +0700

    tai len

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use DB;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/students', function(){
+    //sử dụng query builder
+   return DB::table('students')->where('id','=', 1)->get();
+
+});
+// giá trị truyền vào url sẽ tương ứng vị trí tham số của function
+Route::get('/student/{id}', function ($id, $number){
+    dd('giá trị truyền vào trên url:'.$id . ' ' . $number );
+});
+
+Route::get('/students/detail', function(){
+    return view('students/detail');
+});
+//cách 2
+Route::view('/students/detail-2', 'students.detail');
