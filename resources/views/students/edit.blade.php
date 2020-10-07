da0759b7d2a17673a68a06fdfc3e51d979311f95
hungnbph <hung19993112@gmail.com>
2020-10-07 18:06:09 +0700

    views edit

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -4,48 +4,68 @@
 
 @section('content-header', "Edit student  $student->name")
 @section('content')
-    <form
-        method="POST"
-        action="{{route('students.update', $student->id) }}"
-    >
-        <!-- Them token gui len -->
-        @csrf
+<form
+method="POST"
+        action="{{route('students.update', $student->id) }}">
+         <!-- Them token gui len -->
+         @csrf
         <!-- Thay doi phuong thuc gui request thanh PUT -->
         <input type='hidden' name='_method' value='PUT' />
-        <div>
-            <label for='name'>Name</label>
-            <input id='name' type="text" name="name" value="{{$student->name}}" />
-        </div>
-        <div>
-            <label for='phone'>Phone</label>
-            <input id='phone' type="text" name="phone" value="{{$student->phone}}" />
-        </div>
-        <div>
-            <label for='age'>Age</label>
-            <input id='age' type="number" name="age" value="{{$student->age}}" />
-        </div>
-        <div>
-            <label for='address'>Address</label>
-            <input id='address' type="text" name="address" value="{{$student->address}}" />
-        </div>
-        <div>
-            <label>Gender</label>
-            Nu
-            <input type="radio" name="gender" value="0" {{$student->gender === 0 ? "checked" : ""}} />
-            Nam
-            <input type="radio" name="gender" value="1" {{$student->gender === 1 ? "checked" : ""}} />
-            Khong xac dinh
-            <input type="radio" name="gender" value="2" {{$student->gender === 2 ? "checked" : ""}} />
-        </div>
-        <div>
-            <label>Status</label>
-            Deactive
-            <input type="radio" name="is_active" value="0" {{$student->is_active === 0 ? "checked" : ""}} />
-            Active
-            <input type="radio" name="is_active" value="1" {{$student->is_active === 1 ? "checked" : ""}} />
-        </div>
-        <div>
-            <button type='submit'>UPDATE</button>
-        </div>
-    </form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="name">name</label>
+      <input type="email" class="form-control" id="name" name="name"  value="{{$student->name}}" >
+    </div>
+    <div class="form-group col-md-6">
+      <label for="phone">phone</label>
+      <input type="text" class="form-control" name="phone" id="phone" value="{{$student->phone}}" >
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="age">age</label>
+    <input type="text" class="form-control" id="age" name="age" value="{{$student->age}}" >
+  </div>
+  <div class="form-group">
+    <label for="address">Address</label>
+    <input type="text" class="form-control" name="adress" id="address" value="{{$student->adress}}">
+  </div>
+
+
+  
+  <div class="form-group">
+  <label for="gender">gender</label>
+  <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="gender" value="0" {{$student->gender === 0 ? "checked" : ""}}>
+  <label class="form-check-label" for="nam">nam</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="gender" value="1" {{$student->gender === 1 ? "checked" : ""}}>
+  <label class="form-check-label" for="nu">nữ</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="gender" value="2" {{$student->gender === 2 ? "checked" : ""}}>
+  <label class="form-check-label" for="xd">không xác định</label>
+</div>
+</div>
+
+
+<div class="form-group">
+  <label for="status">status</label>
+  <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="is_active" value="0" {{$student->gender === 0 ? "checked" : ""}}>
+  <label class="form-check-label" for="inlineRadio1">Deactive</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="is_active" value="1" {{$student->gender === 1 ? "checked" : ""}}>
+  <label class="form-check-label" for="inlineRadio2">Active</label>
+</div>
+</div> 
+
+
+
+
+  <button type="submit" class="btn btn-primary">UPDATE</button>
+</form>
+
+    
 @endsection
