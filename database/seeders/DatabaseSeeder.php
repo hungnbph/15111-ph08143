8e02627f7ea1264f66d3220de54c42ed27b730a0
hungnbph <hung19993112@gmail.com>
2020-10-08 10:36:00 +0700

    db new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\CategoryPost;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Post::factory(10)->create();
+        Comment::factory(10)->create();
+        Category::factory(10)->create();
+        CategoryPost::factory(10)->create();Post::factory(10)->create();
+        Comment::factory(10)->create();
+        Category::factory(10)->create();
+        CategoryPost::factory(10)->create();
         // User::factory(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/post', function () {
+    $post = \App\Models\Post::find(2);
+    $comment = \App\Models\Comment::find(1);
+    dd($comment->post);
+});
